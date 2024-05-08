787482c6e8e2311bbb17eead1f76a8aaf6a8819f
mrtoxa42 <betebet16@hotmail.com>
2024-05-08 20:52:09 +0300

    f
    f

--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -4,5 +4,6 @@ var zombie = preload("res://Scenes/zombie.tscn")
 
 func _on_spawn_timer_timeout():
 	var Zombie = zombie.instantiate()
-	get_tree().get_root().add_child(Zombie)
+	#get_tree().get_root().add_child(Zombie)
+	add_child(Zombie)
 	#$ZombieSpawner/SpawnTimer.start()
--- Scripts/zombie.gd
+++ Scripts/zombie.gd
@@ -2,9 +2,10 @@ extends CharacterBody2D
 
 var speed = 200
 
-
 func _process(delta):
-	var player = get_owner().get_node("Player")
+	#var player = get_tree().get_nodes_in_group("Player")
+	var root_node = get_tree().root
+	var player = root_node.get_node("MainGame/Player")
 	var direction = (player.global_position - global_position).normalized()
 	velocity = direction * speed
 	move_and_slide()
